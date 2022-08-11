c1d6b22dc94c500cc986db1ebcd6d5781c84ee0a
laptopAntoineRio <antoine.rio@epita.fr>
2022-08-11 12:39:30 +0200

    started the projects display and more explanations

--- header.js
+++ header.js
@@ -2,6 +2,7 @@ const navMenu = document.getElementById('nav-menu');
 const toggleMenu = document.getElementById('toggle-menu');
 const closeMenu = document.getElementById('close-menu');
 const title = document.getElementById('title');
+const title_project = document.getElementById('title-project');
 
 toggleMenu.addEventListener('click', () => {
     navMenu.classList.toggle('show');
@@ -11,6 +12,13 @@ closeMenu.addEventListener('click', () => {
     navMenu.classList.remove('show');
 });
 
-title.addEventListener('click', () => {
-    location.href = 'index.html';
-});
+if (title) {
+    title.addEventListener('click', () => {
+        location.href = 'index.html';
+    });
+}
+else if (title_project) {
+    title_project.addEventListener('click', () => {
+        location.href = '../index.html';
+    });
+}
